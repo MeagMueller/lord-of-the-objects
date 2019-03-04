9cd3dddfc8b38624dd3f9ae457d15520c3be5ddd
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-04 13:28:55 -0600

    Finished

---
+++ src/scripts/lord-of-the-objects.js
@@ -0,0 +1,135 @@
+const students = [
+    {
+        name: "Chris Miller",
+        subject: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        subject: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        subject: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        subject: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        subject: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        subject: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        subject: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        subject: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        subject: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        subject: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        subject: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        subject: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+]
+
+// const createStudentComponent = (students) => {
+//     return `
+//         <div class="student">
+//             <h1 class="xx-large">${name}</h1>
+//             <section class="bordered dashed section--padded">${subject}</section>
+//             <aside class="pushRight">${info}</aside>
+//         </div>
+//     `
+// }
+// const studentContainer = document.querySelector("#container")
+
+// console.log(createStudentComponent())
+
+const createStudentComponent = (students) => {
+    if (students.score >= 60) {
+        return `
+        <div class="student">
+            <h1 class="xx-large passing">${students.name}</h1>
+            <section class="bordered dashed section--padded">${students.subject}</section>
+            <aside class="pushRight">${students.info}</aside>
+        </div>
+    `
+    } else {
+        return `
+        <div class="student">
+            <h1 class="xx-large failing">${students.name}</h1>
+            <section class="bordered dashed section--padded">${students.subject}</section>
+            <aside class="pushRight">${students.info}</aside>
+        </div>
+    `
+    }
+
+}
+
+const studentContainer = document.querySelector("#container")
+
+for (const student of students) {
+    let studentComponent = ""
+    // if (student.score >= 60) {
+    //     studentComponent = createStudentComponent(
+    //         student.name,
+    //         student.subject,
+    //         student.info,
+    //         "passing"
+    //     )
+    // } else {
+    //     studentComponent = createStudentComponent(
+    //         student.name,
+    //         student.subject,
+    //         student.info,
+    //         "failing"
+    //     )
+    // }
+    studentComponent = createStudentComponent(student)
+    studentContainer.innerHTML += studentComponent
+}
+
+
+
+
